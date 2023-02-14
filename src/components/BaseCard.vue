7acab0c249ce31e3346ea60af6cffd74b69f7b9c
Dimasa9ustian <dimasa9ustian@gmail.com>
2023-02-14 15:48:29 +0700

    refactor(card): make card reusable

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+import BaseCardTitle from "@/components/BaseCardTitle.vue";
+import BaseCardBody from "@/components/BaseCardBody.vue";
+
+
+    defineProps({
+        to: {
+            type: Object,
+            default: undefined,
+        },
+    });
+</script>
+
+<template>
+    <RouterLink v-info="to" :to="to" class="block bg-white shadow p-4 roundedselect-none">
+
+        <BaseCardTitle v-if="!!$slots.title">
+            <slot name="title"> </slot>
+        </BaseCardTitle>
+
+        <BaseCardBody>
+            <slot></slot>
+        </BaseCardBody>
+
+    </RouterLink>
+</template>
