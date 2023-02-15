b8f1c50f0cf11bd5658fc4d0341f071641bc7545
Dimasa9ustian <dimasa9ustian@gmail.com>
2023-02-15 09:43:00 +0700

    refactor: make container as component

--- src/views/RestoDetailView.vue
+++ src/views/RestoDetailView.vue
@@ -2,6 +2,8 @@
     import { useRestoRepository } from "@/composables";
     import { ref, onMounted } from "vue";
     import { useRoute } from "vue-router";
+    import BaseCard from "@/components/BaseCard.vue";
+    import BaseContainer from "@/components/BaseContainer.vue";
 
     const repository = useRestoRepository();
     const route = useRoute();
@@ -24,5 +26,7 @@
 </script>
 
 <template>
-  <div>Sedang menampilkan resto dengan ID: {{ route.params.id }}</div>
+  <BaseContainer>
+        <BaseCard>Sedang menampilkan resto dengan ID: {{ route.params.id }}</BaseCard>
+  </BaseContainer>
 </template>
--- src/views/RestoView.vue
+++ src/views/RestoView.vue
@@ -2,6 +2,8 @@
 import { useRestoRepository } from "@/composables";
 import { ref, onMounted } from "vue";
 import BaseCard from "../components/BaseCard.vue";
+import BaseContainer from "../components/BaseContainer.vue";
+
 
   const repository = useRestoRepository();
 
@@ -28,17 +30,14 @@ import BaseCard from "../components/BaseCard.vue";
   </script>
 
   <template>
-    <div class="min-h-screen container mx-auto">
-      <div class="grid grid-cols-12 gap-4 py-4">
-        <div v-for="resto in restos" :key="resto.id" class="col-span-4">
-          <!-- Card -->
-          <BaseCard :to="{ name: 'restos-show', params: { id: resto.id } }">
-          <template #title>
-            {{ resto.name }}
-          </template>
-          {{ excerpt(resto.description, 40) }}
-        </BaseCard>
+    <BaseContainer>
+        <div class="grid grid-cols-12 gap-4 py-4">
+          <div v-for="resto in restos" :key="resto.id" class="col-span-4">
+              <BaseCard :to="{ name: 'restos.show', params: { id: resto.id }}">
+                  <template #title> {{ resto.name }} </template>
+                  {{ excerpt(resto.description, 40) }}
+              </BaseCard>
+          </div>
         </div>
-      </div>
-    </div>
+    </BaseContainer>
 </template>
